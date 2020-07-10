bd3a2f2044ef4f0d88e0c32e203b4e0ab16065ea
NuggySantoso <apple@ip-192-168-30-16.ap-southeast-1.compute.internal>
2020-07-10 23:32:00 +0700

    Added API View

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a List of APIView Features"""
+        
