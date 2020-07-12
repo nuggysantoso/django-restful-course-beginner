bfdc9ba742fc749ec5cd1e7a1248a98d0a3a26f3
NuggySantoso <apple@Apples-MBP-2.lan>
2020-07-12 19:57:46 +0700

    Configure Deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v04j^lv8me=1j591lxz46u1v&-a=%-5kr1isa6nkpu^9w3j(n7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -122,3 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+STATIC_ROOT = 'static/'
